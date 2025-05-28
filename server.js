2f70cb9abdd2d38fec9578c07dace0a5d3049ad1
kurmetaubanov <kurmet@gmail.com>
2025-05-28 13:23:21 +0700

    before switch from socket.io to phoenix channels

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const ELIXIR_SERVER_URL = process.env.ELIXIR_SERVER_URL || 'http://localhost:4001';
+const ELIXIR_SERVER_URL = process.env.ELIXIR_SERVER_URL || 'http://localhost:4001/';
 
 app.use(express.json());
 app.use(express.static('public'));
